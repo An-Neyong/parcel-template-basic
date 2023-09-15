9a207180f834c3320dbe32901ae22272cbf47ec9
luvMac <cotjdud1010@gmail.com>
2023-09-15 10:54:03 -0700

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,14 @@
+// import
+//impot from 'autoprefixer'
+//const autoprefixer require('autoprefixer')
+// export
+// plugins: [
+//     autoprefixer
+// ]
+
+// 위에 코드를 간소화시킨 코드 
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
